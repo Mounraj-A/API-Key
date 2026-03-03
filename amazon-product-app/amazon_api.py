98575fb015d2971702ef9531e96e98ba023e011b
Mounraj-A <mounraj9025@gmail.com>
2026-03-03 23:45:48 +0530

    Second commit

--- amazon-product-app/amazon_api.py
+++ amazon-product-app/amazon_api.py
@@ -1,56 +1,4 @@
-# # amazon_api.py
-
-# import requests
-# from config import RAPIDAPI_KEY, RAPIDAPI_HOST
-
-
-# def search_products(keyword):
-
-#     url = "https://real-time-amazon-data.p.rapidapi.com/search"
-
-#     querystring = {
-#         "query": keyword,
-#         "country": "IN",
-#         "page": "1",
-#         "sort_by": "RELEVANCE",
-#         "product_condition": "ALL"
-#     }
-
-#     headers = {
-#         "X-RapidAPI-Key": RAPIDAPI_KEY,
-#         "X-RapidAPI-Host": RAPIDAPI_HOST
-#     }
-
-#     response = requests.get(url, headers=headers, params=querystring)
-
-#     if response.status_code != 200:
-#         print("Error:", response.text)
-#         return []
-
-#     data = response.json()
-
-#     products = []
-
-#     if "data" in data and "products" in data["data"]:
-
-#         for item in data["data"]["products"]:
-
-#             product = {
-#                 "title": item.get("product_title", "No Title"),
-#                 "image": item.get("product_photo", ""),
-#                 "price": item.get("product_price", "Not Available"),
-#                 "old_price": item.get("product_original_price", ""),
-#                 "rating": item.get("product_star_rating", "No Rating"),
-#                 "reviews": item.get("product_num_ratings", "0"),
-#                 "offer": item.get("product_badge", ""),
-#                 "url": item.get("product_url", "#")
-#             }
-
-#             products.append(product)
-#     return products
-
-
-# amazon_api.py  (you can rename later)
+# amazon_api.py
 
 import requests
 from config import RAPIDAPI_KEY, RAPIDAPI_HOST
@@ -58,12 +6,14 @@ from config import RAPIDAPI_KEY, RAPIDAPI_HOST
 
 def search_products(keyword):
 
-    url = "https://real-time-flipkart-data2.p.rapidapi.com/product-search"
+    url = "https://real-time-amazon-data.p.rapidapi.com/search"
 
     querystring = {
-        "q": keyword,
+        "query": keyword,
+        "country": "IN",
         "page": "1",
-        "sort_by": "RELEVANCE"
+        "sort_by": "RELEVANCE",
+        "product_condition": "ALL"
     }
 
     headers = {
@@ -78,23 +28,21 @@ def search_products(keyword):
         return []
 
     data = response.json()
-    print("DEBUG RESPONSE:", data)  # remove later
 
     products = []
 
-    # Flipkart response parsing
     if "data" in data and "products" in data["data"]:
 
         for item in data["data"]["products"]:
 
             product = {
-                "title": item.get("title", "No Title"),
-                "image": item.get("images", [""])[0] if item.get("images") else "",
-                "price": item.get("price", "Not Available"),
-                "old_price": item.get("original_price", ""),
-                "rating": item.get("rating", "No Rating"),
-                "reviews": item.get("rating_count", "0"),
-                "offer": item.get("discount", ""),
+                "title": item.get("product_title", "No Title"),
+                "image": item.get("product_photo", ""),
+                "price": item.get("product_price", "Not Available"),
+                "old_price": item.get("product_original_price", ""),
+                "rating": item.get("product_star_rating", "No Rating"),
+                "reviews": item.get("product_num_ratings", "0"),
+                "offer": item.get("product_badge", ""),
                 "url": item.get("product_url", "#")
             }
 
